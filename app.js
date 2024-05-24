86cf29af6ae1bce9b34da0236ede644a859e8cbc
erdison <109794675+Soni912108@users.noreply.github.com>
2024-05-24 16:57:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,9 +42,6 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(3001, () => {
-      console.log("Server is running on port 3001");
-    });
   })
   .catch(() => {
     console.log("Connection failed!");
