7eca6c16d296b0e3887e74472ba66ccdb7654326
erdison <109794675+Soni912108@users.noreply.github.com>
2024-05-28 14:29:48 +0200

    Update tasks.model.js

--- models/tasks.model.js
+++ models/tasks.model.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 
 // initializing a simple mongoose Schema to represent the products
-const TaskSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please enter task name"],
-    },
-
-    description: {
-      type: String,
-      required: true,
-    },
-    completed: {
-      type: Boolean,
-      required: true,
-    },
+const TaskSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter task name"],
   },
-  {
-    timestamps: true,
-  }
-);
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference the User model
+  },
+}, {
+  timestamps: true,
+});
+
 
 
 const Tasks = mongoose.model("Tasks", TaskSchema);
