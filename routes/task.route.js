c21a91f16477ac54764d05bc714148f6e0174ae3
erdison <109794675+Soni912108@users.noreply.github.com>
2024-05-28 14:30:56 +0200

    Update task.route.js

--- routes/task.route.js
+++ routes/task.route.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const {getTasks, getTask, createTask, updateTask, deleteTask} = require('../controllers/tasks.controller.js');
 
 
-router.get('/', getTasks);
+// Route for fetching tasks for a specific user
+router.get('/:userId', getTasks);
+
 router.get("/:id", getTask);
 
 router.post("/", createTask);
