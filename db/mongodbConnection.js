42cc44d4f15e6cc424dbf2a685e342442576ef0e
erdison <109794675+Soni912108@users.noreply.github.com>
2024-09-15 13:03:29 +0200

    Update mongodbConnection.js

--- db/mongodbConnection.js
+++ db/mongodbConnection.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 const connectToMongoDB = async () => {
 
-  const password = process.env.PASSWORD;
-  const encodedPassword = encodeURIComponent(password);
-  let MONGODB_URI = `mongodb+srv://sonimailfortestuse:${encodedPassword}@backenddb.yf5ipol.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`
+  // const password = process.env.PASSWORD;
+  // const encodedPassword = encodeURIComponent(password);
+  // let MONGODB_URI = `mongodb+srv://sonimailfortestuse:${encodedPassword}@backenddb.yf5ipol.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`
 
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.info('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
