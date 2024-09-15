997359dc28e1a293b65b22c6b571f58ed650d5f6
erdison <109794675+Soni912108@users.noreply.github.com>
2024-09-15 13:57:36 +0200

    Update mongodbConnection.js

--- db/mongodbConnection.js
+++ db/mongodbConnection.js
@@ -20,32 +20,3 @@ const connectToMongoDB = async () => {
 };
 
 module.exports = connectToMongoDB;
-
-
-
-
-
-
-
-
-
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-
-
-// const connectToMongoDB = async () => {
-
-//   // const password = process.env.PASSWORD;
-//   const mongoURI = process.env.MONGODB_URI;
-//   // const encodedPassword = encodeURIComponent(password);
-//   // let MONGODB_URI = `mongodb+srv://sonimailfortestuse:${encodedPassword}@backenddb.yf5ipol.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB`
-
-//   try {
-//     await mongoose.connect(mongoURI);
-//     console.info('Connected to MongoDB');
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//   }
-// };
-
-// module.exports = connectToMongoDB;
