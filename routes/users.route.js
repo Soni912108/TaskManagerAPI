35bdc04f5845b111eaeb2b64429e3cfe8a2502a9
erdison <109794675+Soni912108@users.noreply.github.com>
2025-10-24 12:41:15 +0200

    Clarify user authentication routes with comments
    Added comments to clarify user authentication routes.

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,9 +2,9 @@ const express = require("express");
 const {Register,Login} = require('../controllers/users.controller.js');
 const userRouter = express.Router();
 
-
-userRouter.post('/register', Register); // Use POST for user creation
-
-userRouter.post('/login', Login); // Use post for user login
+//routes for users auth, login and register.
+userRouter.post('/register', Register);
+userRouter.post('/login', Login);
 
 module.exports = userRouter;
+
